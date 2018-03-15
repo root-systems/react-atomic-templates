a204b71e8000b98e48e83829b6be9de3f592fc02
Daniel Lewis <dan@rootsystems.nz>
2018-03-15 20:03:07 +1300

    update ref to new components

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 
-import Example from '../../src'
+import { SectionFullWidth, SectionStandard} from '../../src'
 
 class Demo extends Component {
   render() {
     return <div>
       <h1>layout Demo</h1>
-      <Example/>
     </div>
   }
 }
